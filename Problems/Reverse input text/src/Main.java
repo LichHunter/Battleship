bf4baa465cf578825a18569aa1ee13da123675b2
AlexD2019 <alexander0derevianko@gmail.com>
2021-01-13 11:48:16 +0000

    Problem: Reverse input text

---
+++ Problems/Reverse input text/src/Main.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		// start coding here
+
+		System.out.println(new StringBuilder().append(reader.readLine()).reverse());
+
+		reader.close();
+	}
+}
